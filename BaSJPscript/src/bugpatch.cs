a7bd8d170d13f50ec6dcc6fa6be61d44691bda05
S-Katagiri <katagiri.stsh@gmail.com>
2023-04-08 18:57:23 +0900

    a

---
+++ BaSJPscript/src/bugpatch.cs
@@ -0,0 +1,33 @@
+﻿using ThunderRoad;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace BaS_JP
+{
+    public class BASJP : ThunderScript
+    {
+        public override void ScriptLoaded(ModManager.ModData modData)
+        {
+            base.ScriptLoaded(modData);
+        }
+        public override void ScriptEnable()
+        {
+            ModManager.OnModLoad += ModManager_OnModLoad;
+            base.ScriptEnable();
+        }
+        public override void ScriptDisable()
+        {
+            ModManager.OnModLoad -= ModManager_OnModLoad;
+            base.ScriptDisable();
+        }
+        private void ModManager_OnModLoad(EventTime eventTime, ModManager.ModLoadEventType eventType, ModManager.ModData modData = null)
+        {
+            if(modData == null)
+            {
+                string currentLanguage = LocalizationManager.Instance.Language;
+                EventManager.InvokeLanguageChanged("Bruh");
+                EventManager.InvokeLanguageChanged(currentLanguage);
+            }
+        }
+    }
+}
